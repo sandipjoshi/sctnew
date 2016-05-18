bf68feca40017c796af2e72d1ee80ebd883a0980
Parth pokar <parthishepy@gmail.com>
2016-05-18 16:32:45 +0530

    cha

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = mysqli_connect("localhost", "root", "", "SchoolTime");
+$mysqli = mysqli_connect("localhost", "root", "SCT@123", "SchoolTime");
 if (mysqli_connect_errno($mysqli)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
