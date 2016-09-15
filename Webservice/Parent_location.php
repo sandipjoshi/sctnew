f2a747408f2b13812a33e897c4aa932770532c09
root <root@ip-172-31-29-44.ap-southeast-1.compute.internal>
2016-09-15 12:09:28 +0000

    before pem

--- Webservice/Parent_location.php
+++ Webservice/Parent_location.php
@@ -3,7 +3,9 @@ include"../config/config.php";
 $mobile_no=$_POST['mob_no'];
 $lat=$_POST['lat'];
 $long=$_POST['long'];
-$sql = "UPDATE `Parent` SET `lat`='$lat',`long`='$long' WHERE MobileNo='$mobile_no'" or die("quiery is not working") ;
+$device=$_POST['device_id'];
+$type=$_POST['type'];
+$sql = "UPDATE `Parent` SET `lat`='$lat',`long`='$long',`device_id`='$device',`type`='$type' WHERE MobileNo='$mobile_no'" or die("quiery is not working") ;
 
 if ($mysqli->query($sql) === TRUE) {
     $true=array('Success'=>'YES');
